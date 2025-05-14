1aa96b93ac0e90bd2fe93a665779e830bb8fc472
flippette <me@flippo.cc>
2025-05-14 20:04:48 +0300

    make inlay hint background transparent

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,6 +10,13 @@ return {
         unknown = "",
       },
     },
+    config = function(_, opts)
+      require("lsp-endhints").setup(opts)
+      local inlay_hl =
+        vim.api.nvim_get_hl(0, { name = "LspInlayHint" })
+      inlay_hl.bg = "NONE"
+      vim.api.nvim_set_hl(0, "LspInlayHint", inlay_hl)
+    end,
   },
   {
     "mrcjkb/rustaceanvim",
