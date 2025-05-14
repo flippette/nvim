1126365f2a4f00f1d9e11fae3649f8e7d1980b6e
flippette <me@flippo.cc>
2025-05-14 03:32:00 +0300

    remove mason pin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,4 @@
 return {
-  { "mason-org/mason.nvim", version = "1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
   {
     "chrisgrieser/nvim-lsp-endhints",
     event = "LspAttach",
