2d1cd559c44951d9752d205349175e5def560d4c
flippette <me@flippo.cc>
2025-06-13 00:11:03 +0700

    2-space indents!!!

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,6 +9,12 @@ vim.o.guicursor = ""
 -- Disable animations
 vim.g.snacks_animate = false
 
+-- 2-space indenting by default
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+
 -- Auto-updates
 vim.api.nvim_create_autocmd("VimEnter", {
   group = vim.api.nvim_create_augroup(
