47e63816e529778cace6f031f21a8b6b309f6909
flippette <me@flippo.cc>
2025-09-01 23:49:04 +0300

    scala DAP

--- lua/plugins/lang/scala.lua
+++ lua/plugins/lang/scala.lua
@@ -28,5 +28,25 @@ return {
       end,
       group = augroup,
     })
+
+    local dap = require("dap")
+    dap.configurations.scala = {
+      {
+        type = "scala",
+        request = "launch",
+        name = "RunOrTest",
+        metals = {
+          runType = "runOrTestFile",
+        },
+      },
+      {
+        type = "scala",
+        request = "launch",
+        name = "Test Target",
+        metals = {
+          runType = "testTarget",
+        },
+      },
+    }
   end,
 }
