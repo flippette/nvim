20fe66b2b696cc1b49a0dfa772c159bc5cdb25b3
flippette <me@flippo.cc>
2025-09-12 19:53:33 +0300

    make sure scala TS grammar is installed

--- lua/plugins/lang/scala.lua
+++ lua/plugins/lang/scala.lua
@@ -1,52 +1,60 @@
 return {
-  "scalameta/nvim-metals",
-  ft = { "scala", "sbt" },
-  dependencies = {
-    "mfussenegger/nvim-dap",
-  },
-  opts = function()
-    local metals = require("metals")
-    local config = metals.bare_config()
-    config.on_attach = metals.setup_dap()
-    config.settings = {
-      showImplicitArguments = true,
-      superMethodLensesEnabled = true,
-      useGlobalExecutable = true,
-    }
-    return config
-  end,
-  config = function(self, config)
-    local metals = require("metals")
-    local augroup = vim.api.nvim_create_augroup(
-      "nvim-metals",
-      { clear = true }
-    )
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = self.ft,
-      callback = function()
-        metals.initialize_or_attach(config)
-      end,
-      group = augroup,
-    })
+  {
+    "scalameta/nvim-metals",
+    ft = { "scala", "sbt" },
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    opts = function()
+      local metals = require("metals")
+      local config = metals.bare_config()
+      config.on_attach = metals.setup_dap()
+      config.settings = {
+        showImplicitArguments = true,
+        superMethodLensesEnabled = true,
+        useGlobalExecutable = true,
+      }
+      return config
+    end,
+    config = function(self, config)
+      local metals = require("metals")
+      local augroup = vim.api.nvim_create_augroup(
+        "nvim-metals",
+        { clear = true }
+      )
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          metals.initialize_or_attach(config)
+        end,
+        group = augroup,
+      })
 
-    local dap = require("dap")
-    dap.configurations.scala = {
-      {
-        type = "scala",
-        request = "launch",
-        name = "RunOrTest",
-        metals = {
-          runType = "runOrTestFile",
+      local dap = require("dap")
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+          },
         },
-      },
-      {
-        type = "scala",
-        request = "launch",
-        name = "Test Target",
-        metals = {
-          runType = "testTarget",
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
         },
-      },
-    }
-  end,
+      }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "scala" },
+    },
+  },
 }
