223182783c2b913b69ab74e3ebaf4da8e113f45c
flippette <me@flippo.cc>
2025-09-17 17:11:01 +0300

    upgrade to lazyvim 15

--- lua/plugins/line.lua
+++ lua/plugins/line.lua
@@ -1,38 +1,25 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        component_separators = "",
-        section_separators = "",
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      component_separators = "",
+      section_separators = "",
+    },
+    sections = {
+      lualine_a = {
+        {
+          function()
+            return " "
+          end,
+          padding = 0,
+        },
       },
-      sections = {
-        lualine_a = {
-          {
-            function()
-              return " "
-            end,
-            padding = 0,
-          },
-        },
-        lualine_y = {
-          { "progress", padding = 1 },
-        },
-        lualine_z = {
-          { "location", padding = 1 },
-        },
+      lualine_y = {
+        { "progress", padding = 1 },
+      },
+      lualine_z = {
+        { "location", padding = 1 },
       },
     },
-  },
-
-  {
-    "akinsho/bufferline.nvim",
-    init = function()
-      local catppuccin =
-        require("catppuccin.groups.integrations.bufferline")
-      function catppuccin.get()
-        return catppuccin.get_theme()
-      end
-    end,
   },
 }
