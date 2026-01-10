c0f29f852e0dab17e8007256c19fcaa814970368
flippette <me@flippo.cc>
2026-01-10 02:21:51 +0200

    nix support

--- lua/plugins/lang/nix.lua
+++ lua/plugins/lang/nix.lua
@@ -1,10 +1,27 @@
 return {
+  recommended = {
+    ft = "nix",
+    root = {
+      "flake.nix",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "nix" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nixd = {},
+      },
+    },
+  },
   {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "alejandra",
-        -- "statix",
       },
     },
   },
@@ -13,6 +30,14 @@ return {
     opts = {
       formatters_by_ft = {
         nix = { "alejandra" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        nix = { "statix" },
       },
     },
   },
