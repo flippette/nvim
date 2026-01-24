93d3d3b5eea322d205623695ac583a507086fdda
flippette <me@flippo.cc>
2026-01-25 00:08:33 +0200

    drop haskell tools & hoogle

--- lua/plugins/lang/haskell.lua
+++ lua/plugins/lang/haskell.lua
@@ -11,81 +11,10 @@ return {
   },
 
   {
-    "mrcjkb/haskell-tools.nvim",
-    version = false,
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-    keys = {
-      {
-        "<localleader>e",
-        "<cmd>HlsEvalAll<cr>",
-        ft = "haskell",
-        desc = "Evaluate All",
-      },
-      {
-        "<localleader>h",
-        function()
-          require("haskell-tools").hoogle.hoogle_signature()
-        end,
-        ft = "haskell",
-        desc = "Hoogle Signature",
-      },
-      {
-        "<localleader>r",
-        function()
-          require("haskell-tools").repl.toggle()
-        end,
-        ft = "haskell",
-        desc = "REPL (Package)",
-      },
-      {
-        "<localleader>R",
-        function()
-          require("haskell-tools").repl.toggle(
-            vim.api.nvim_buf_get_name(0)
-          )
-        end,
-        ft = "haskell",
-        desc = "REPL (Buffer)",
-      },
-    },
-  },
-
-  {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
-        "haskell-debug-adapter",
         "fourmolu",
-      },
-    },
-  },
-
-  {
-    "luc-tielen/telescope_hoogle",
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-    opts = function()
-      LazyVim.on_load("telescope.nvim", function()
-        require("telescope").load_extension("ht")
-      end)
-    end,
-    keys = {
-      {
-        "<localleader>H",
-        "<cmd>Telescope hoogle<cr>",
-        ft = "haskell",
-        desc = "Hoogle",
-      },
-    },
-  },
-
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      {
-        "mason-org/mason.nvim",
-        opts = {
-          ensure_installed = { "haskell-debug-adapter" },
-        },
       },
     },
   },
